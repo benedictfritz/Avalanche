13a21095a7c6f3c84c38633106daff89d39acc57
Benedict Fritz <benedictfritz@gmail.com>
2010-02-22 21:15:05 -0600

    Pointed the Score.js towards the proper servlet for submitting high scores
    Signed-off-by: Benedict Fritz <benedictfritz@gmail.com>

--- Score.js
+++ Score.js
@@ -1,8 +1,8 @@
 function submitScore() {
   var highScore = document.getElementById("HighScore").innerHTML;
   var name = document.getElementById("name").value;
-  var url = "http://localhost:8888/sign";
-  //var url = "http://avalanchescores.appspot.com/sign";
+  //var url = "http://localhost:8888/sign";
+  var url = "http://avalanchescores.appspot.com/sign";
   var parameters = "score=" + highScore + "&name=" + name;
   var req = new XMLHttpRequest();
   req.open("POST", url, true);
