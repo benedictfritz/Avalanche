c317e6ba7917568dd0d24f32655ce7ff23cb464d
Benedict Fritz <benedictfritz@gmail.com>
2010-02-22 21:03:04 -0600

    Added high score server interaction
    Signed-off-by: Benedict Fritz <benedictfritz@gmail.com>

---
+++ Score.js
@@ -0,0 +1,11 @@
+function submitScore() {
+  var highScore = document.getElementById("HighScore").innerHTML;
+  var name = document.getElementById("name").value;
+  var url = "http://localhost:8888/sign";
+  //var url = "http://avalanchescores.appspot.com/sign";
+  var parameters = "score=" + highScore + "&name=" + name;
+  var req = new XMLHttpRequest();
+  req.open("POST", url, true);
+  req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  req.send(parameters);
+}
