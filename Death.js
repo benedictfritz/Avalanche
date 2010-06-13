781c97caf9a6a5344f8d71c0464a5071ab2e2f05
unknown <Alex@.(none)>
2010-06-13 16:24:53 -0700

    Added a death screen. The death screen needs space bar to be pressed to continue

---
+++ Death.js
@@ -0,0 +1,15 @@
+function Death()
+{
+  this.startupDeath = function()
+  {
+    this.startupVisualGameObject(g_ResourceManager.death, 0, 0, 1);
+    return this;
+  }
+
+  this.keyDown = function(event)
+  {
+    if (event.keyCode == 32)
+		g_ApplicationManager.startLevel();
+  }
+}
+Death.prototype = new VisualGameObject;
